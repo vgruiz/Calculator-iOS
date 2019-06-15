8aebdeaec3ffbfdcbcf24d509e4dc4c02346cdd9
Victor Ruiz <vgruiz95@gmail.com>
2019-06-14 20:33:52 -0700

    Improved control flow and some issues with updating the UI.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBOutlet weak var primaryLabel: UILabel!
+
+    //MARK: IBOutlet declarations
     //TODO: add a secondaryLabel that displays operandA (and maybe the operator) when available
+    @IBOutlet weak var primaryLabel: UILabel!
     @IBOutlet weak var equalButton: UIButton!
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minusButton: UIButton!
@@ -34,26 +35,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var nineButton: UIButton!
     
     //MARK: Global Vars
-    var operatorSelected : Bool = false
+    var buttons : [UIButton]!
     var operators : [Bool] = [false, false, false, false]
                         //   /      x       -      +
     var operandA : Float!
     var operandB : Float!
-    var operandChooser : Bool = true
-    //true if you're editing operandA, false if you're editing operandB
-    
+    var operandChooser : Bool = true //true if you're editing operandA, false if you're editing operandB
+    var operatorSelected : Bool = false
+    var marker : Bool  = false
     var marker2 : Bool = false
-    var buttons : [UIButton]!
+    var decimalUsed : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         buttons = [equalButton, plusButton, minusButton, multiplicationButton, divisionButton, percentButton, plusMinusButton, ACButton, decimalButton, zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton]
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        
     }
     
     override func viewDidLayoutSubviews() {
@@ -64,6 +60,8 @@ class ViewController: UIViewController {
         }
     }
 
+    //MARK: IBAction methods for non-operand UIButtons
+    
     @IBAction func ACButtonPressed(_ sender: UIButton) {
         primaryLabel.text = "0"
         operatorSelected = false
@@ -74,22 +72,6 @@ class ViewController: UIViewController {
         operandB = nil
         operandChooser = true
     }
-    
-    func clearOperators() {
-        for i in 0...operators.count - 1 {
-           operators[i] = false
-        }
-        divisionButton.backgroundColor = .orange
-        divisionButton.setTitleColor(.white, for: .normal)
-        multiplicationButton.backgroundColor = .orange
-        multiplicationButton.setTitleColor(.white, for: .normal)
-        minusButton.backgroundColor = .orange
-        minusButton.setTitleColor(.white, for: .normal)
-        plusButton.backgroundColor = .orange
-        plusButton.setTitleColor(.white, for: .normal)
-    }
-    
-    //MARK: Non-Operands
     @IBAction func plusMinusButton(_ sender: UIButton) {
         primaryLabel.text = "-" + primaryLabel.text!
     }
@@ -119,41 +101,8 @@ class ViewController: UIViewController {
             operatorSelected(operatorID: 3)
         }
     }
-    func operatorSelected(operatorID: Int) {
-        operatorSelected = true
-        setOperator(index: operatorID)
-        operandA = getOperand()
-        operandChooser = false
-        
-        switch(operatorID) {
-        case 0:
-            divisionButton.backgroundColor = .white
-            divisionButton.setTitleColor(.orange, for: .normal)
-        case 1:
-            multiplicationButton.backgroundColor = .white
-            multiplicationButton.setTitleColor(.orange, for: .normal)
-        case 2:
-            minusButton.backgroundColor = .white
-            minusButton.setTitleColor(.orange, for: .normal)
-        case 3:
-            plusButton.backgroundColor = .white
-            plusButton.setTitleColor(.orange, for: .normal)
-        default:
-            let x : Int = 1
-        }
-    }
-    
-    func setOperator(index: Int) {
-        for i in 0...operators.count - 1 {
-            if i == index {
-                operators[i] = true
-            } else {
-                operators[i] = false
-            }
-        }
-    }
     @IBAction func equalButton(_ sender: UIButton) {
-        //Interpret()
+        
         operandB = getOperand()
         
         var operatorSelect : Int = -1
@@ -162,6 +111,11 @@ class ViewController: UIViewController {
             if operators[i] == true {
                 operatorSelect = i
             }
+        }
+        
+        if operatorSelect == -1 {
+            operandB = nil
+            return
         }
         
         var solution : Float!
@@ -185,12 +139,127 @@ class ViewController: UIViewController {
         
         clearOperators()
         displaySolution(solution: solution)
-        
         operandA = solution
         operandB = nil
         operandChooser = false
         marker = false
         marker2 = true
+        operatorSelected = false
+    }
+    
+
+    
+    //MARK: IBAction methods for operand buttons
+    @IBAction func decimalButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: ".")
+    }
+    @IBAction func zeroButton(_ sender: UIButton) {
+        //TODO: add logic to remove leading zeros
+        updateOperators()
+        addSymbol(symbol: "0")
+    }
+    @IBAction func oneButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "1")
+    }
+    @IBAction func twoButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "2")
+    }
+    @IBAction func threeButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "3")
+    }
+    @IBAction func fourButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "4")
+    }
+    @IBAction func fiveButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "5")
+    }
+    @IBAction func sixButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "6")
+    }
+    @IBAction func sevenButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "7")
+    }
+    @IBAction func eightButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "8")
+    }
+    @IBAction func nineButton(_ sender: UIButton) {
+        updateOperators()
+        addSymbol(symbol: "9")
+    }
+    
+    //MARK: Functions for control flow and updating the UI
+    func clearOperators() {
+        for i in 0...operators.count - 1 {
+            operators[i] = false
+        }
+        divisionButton.backgroundColor = .orange
+        divisionButton.setTitleColor(.white, for: .normal)
+        multiplicationButton.backgroundColor = .orange
+        multiplicationButton.setTitleColor(.white, for: .normal)
+        minusButton.backgroundColor = .orange
+        minusButton.setTitleColor(.white, for: .normal)
+        plusButton.backgroundColor = .orange
+        plusButton.setTitleColor(.white, for: .normal)
+    }
+    
+    func operatorSelected(operatorID: Int) {
+        
+        //this is so if you enter "1 + 1", if you hit another operator, it sums the "1 + 1" but allows you to continue the expression
+        if operatorSelected {
+            if operandB == nil {
+                clearOperators()
+                setOperator(index: operatorID)
+                
+            } else {
+                //solve the previous expression and set it to operandA
+                self.equalButton(UIButton())
+                //set the operator colors
+                updateOperators()
+            }
+            
+        } else {
+            operatorSelected = true
+            setOperator(index: operatorID)
+            operandA = getOperand()
+            operandChooser = false
+        }
+        
+        switch(operatorID) {
+        case 0:
+            divisionButton.backgroundColor = .white
+            divisionButton.setTitleColor(.orange, for: .normal)
+        case 1:
+            multiplicationButton.backgroundColor = .white
+            multiplicationButton.setTitleColor(.orange, for: .normal)
+        case 2:
+            minusButton.backgroundColor = .white
+            minusButton.setTitleColor(.orange, for: .normal)
+        case 3:
+            plusButton.backgroundColor = .white
+            plusButton.setTitleColor(.orange, for: .normal)
+        default:
+            let x : Int = 1
+        }
+        
+    }
+    
+    func setOperator(index: Int) {
+        for i in 0...operators.count - 1 {
+            if i == index {
+                operators[i] = true
+            } else {
+                operators[i] = false
+            }
+        }
     }
     
     func displaySolution(solution: Float) {
@@ -203,49 +272,16 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func decimalButton(_ sender: UIButton) {
-        //TODO: add logic to check for a dot before adding one
-        addSymbol(symbol: ".")
+    func updateOperators() {
+        divisionButton.backgroundColor = .orange
+        divisionButton.setTitleColor(.white, for: .normal)
+        multiplicationButton.backgroundColor = .orange
+        multiplicationButton.setTitleColor(.white, for: .normal)
+        minusButton.backgroundColor = .orange
+        minusButton.setTitleColor(.white, for: .normal)
+        plusButton.backgroundColor = .orange
+        plusButton.setTitleColor(.white, for: .normal)
     }
-    
-    //MARK: Operands
-    @IBAction func zeroButton(_ sender: UIButton) {
-        //TODO: add logic to remove leading zeros
-        addSymbol(symbol: "0")
-    }
-    @IBAction func oneButton(_ sender: UIButton) {
-        addSymbol(symbol: "1")
-    }
-    @IBAction func twoButton(_ sender: UIButton) {
-        addSymbol(symbol: "2")
-    }
-    @IBAction func threeButton(_ sender: UIButton) {
-        addSymbol(symbol: "3")
-    }
-    @IBAction func fourButton(_ sender: UIButton) {
-        addSymbol(symbol: "4")
-    }
-    @IBAction func fiveButton(_ sender: UIButton) {
-        addSymbol(symbol: "5")
-    }
-    @IBAction func sixButton(_ sender: UIButton) {
-        addSymbol(symbol: "6")
-    }
-    @IBAction func sevenButton(_ sender: UIButton) {
-        addSymbol(symbol: "7")
-    }
-    @IBAction func eightButton(_ sender: UIButton) {
-        addSymbol(symbol: "8")
-    }
-    @IBAction func nineButton(_ sender: UIButton) {
-        addSymbol(symbol: "9")
-    }
-    
-
-    var inInitState : Bool = true
-    
-    var marker : Bool  = false
-    var decimalUsed : Bool = false
 
     func addSymbol(symbol : String) {
         
